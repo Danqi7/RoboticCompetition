c6499c97676dac4bb35a68e10621f316c4201f1e
Danqi Liao <danqiliao@gmail.com>
2016-05-13 13:07:00 -0500

    added comments

--- robotic_2.0/robotic_2.0.ino
+++ robotic_2.0/robotic_2.0.ino
@@ -2,16 +2,16 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-
-  pinMode(3, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
+//
+//  pinMode(13, OUTPUT);
+//  pinMode(12, OUTPUT);
+//  pinMode(11, OUTPUT);
+//  pinMode(10, OUTPUT);
+//
+//  pinMode(3, OUTPUT);
+//  pinMode(2, OUTPUT);
+//  pinMode(5, OUTPUT);
+//  pinMode(4, OUTPUT);
 
 }
 
@@ -181,8 +181,9 @@ RoboticCar ourCar;
 
 
 void loop() {
-  // put your main code here, to run repeatedly
-    unsigned long current = millis();
+    // put your main code here, to run repeatedly
+    
+    unsigned long current = millis();//get current time
 
     //while we are running within the boundary
     while (lightA < thLight && lightB < thLight )
